a5a7038e3c1a8e9c723762b488064a722a174e15
Farjana Akhter <farjana.akhter085@gmail.com>
2023-10-20 13:54:51 -0500

    added schema to create User model, and use mongoose middleware to define how the data is returned in  models/User.js

--- models/User.js
+++ models/User.js
@@ -1,41 +1,41 @@
-// const { Schema, model } = require("mongoose");
+const { Schema, model } = require("mongoose");
 
-// const userSchema = new Schema(
-//   {
-//     username: {
-//       type: String,
-//       unique: true,
-//       required: true,
-//       trim: true,
-//     },
-//     email: {
-//       type: String,
-//       required: true,
-//       unique: true,
-//       // match: [/.+@.+\..+/],
-//       match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/],
-//     },
-//     thoughts: [
-//       {
-//         type: Schema.Types.ObjectId,
-//         ref: 'thought',
-//       },
-//     ],
-//     friends: [
-//       {
-//         type: Schema.Types.ObjectId,
-//         ref: 'user',
-//       },
-//     ],
-//   },
-//   {
-//   // toJSON is a mongoose middleware to define how the data is returned
-//     toJSON: {
-//       virtuals: true,
-//     },
-//     id: false,
-//   }
-// );
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      // match: [/.+@.+\..+/],
+      match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/],
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'thought',
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'user',
+      },
+    ],
+  },
+  {
+  // toJSON is a mongoose middleware to define how the data is returned
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
 
 // // Increases the friend count in User model object when friends are added by a user
 // userSchema.virtual("friendCount").get(function () {
@@ -46,4 +46,4 @@
 // const User = model('user', userSchema);
 
 
-// module.exports = User;
+module.exports = User;
