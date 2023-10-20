6c8d53f8d8f307e5ac5ca483717d25053b7b4cef
Farjana Akhter <farjana.akhter085@gmail.com>
2023-10-20 16:28:17 -0500

    added code for handling CRUD (Create, Read, Update, Delete) operations and create route to managing thoughts and their reactions in routes/api/thoughts-routes.js

--- routes/api/thoughts-routes.js
+++ routes/api/thoughts-routes.js
@@ -1,37 +1,37 @@
-// const router = require('express').Router();
+const router = require('express').Router();
 
-// const {
-//     getThought,
-//     getSingleThought,
-//     createThought,
-//     updateThought,
-//     deleteThought,
-//     addReaction, 
-//     removeReaction
-// } = require('../../controllers/thought-controller');
+const {
+    getThought,
+    getSingleThought,
+    createThought,
+    updateThought,
+    deleteThought,
+    addReaction, 
+    removeReaction
+} = require('../../controllers/thought-controller');
 
-// // Set up Get all and Post at /api/thoughts
-// router
-//     .route('/')
-//     .get(getThought)
-//     .post(createThought);
+// Set up Get all and Post at /api/thoughts
+router
+    .route('/')
+    .get(getThought)
+    .post(createThought);
     
-// // Set up Get one, Put, and Delete at /api/thoughts/:id
-// // /api/thoughts/:thoughtId GET one thought, PUT and DELETE by iD
-// router
-//     .route('/:thoughtId')
-//     .get(getSingleThought) 
-//     .put(updateThought)
-//     .delete(deleteThought);
+// Set up Get one, Put, and Delete at /api/thoughts/:id
+// /api/thoughts/:thoughtId GET one thought, PUT and DELETE by iD
+router
+    .route('/:thoughtId')
+    .get(getSingleThought) 
+    .put(updateThought)
+    .delete(deleteThought);
     
-//  //   /api/thoughts/:thoughtId/reactions
+ //   /api/thoughts/:thoughtId/reactions
  
-// router
-//   .route('/:thoughtId/reactions')
-//   .post(addReaction);
+router
+  .route('/:thoughtId/reactions')
+  .post(addReaction);
 
-//   router
-//   .route('/:thoughtId/reactions/:reactionId')
-//   .delete(removeReaction);
+  router
+  .route('/:thoughtId/reactions/:reactionId')
+  .delete(removeReaction);
 
-// module.exports = router;
+module.exports = router;
